ff504ac842964045c6b3e9f29ec8b096808dd6bd
dlenehan <dlenehan@google.com>
2024-11-12 18:04:20 +0000

    update to setup.sh

--- setup.sh
+++ setup.sh
@@ -110,7 +110,7 @@ fi
 PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
 COMMITID=$(git log --format="%H" -n 1)
 SHORTCOMMIT=${COMMITID: -5}
-SERVICE_ACCOUNT=recaptcha-heroes-$SHORTCOMMIT@$PROJECT_NAME.iam.gserviceaccount.com
+SERVICE_ACCOUNT=recaptcha-heroes-$SHORTCOMMIT@$PROJECT_ID.iam.gserviceaccount.com
 LOG_BUCKET=recaptcha-heroes-logs-$SHORTCOMMIT
 
 APIKEY=$(gcloud services api-keys create --api-target=service=recaptchaenterprise.googleapis.com --display-name="reCAPTCHA Heroes Demo API key" --format="json" 2>/dev/null | jq '.response.keyString' | cut -d"\"" -f2)
