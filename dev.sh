c632a8abc38d857d4a1e25c6ebff6081735f2549
dlenehan <dlenehan@google.com>
2024-11-12 17:43:32 +0000

    update to setup.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+select_project () {
+    echo -n "What Project ID do you want to deploy the solution to? (for example my-project-1234): "
+    read var_project_id
+    gcloud config set project $var_project_id
+    export PROJECT_ID=$var_project_id
+    export PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
+}
+
+if [ -z "$GOOGLE_CLOUD_PROJECT" ]
+then
+    echo "Project not set!"
+    select_project
+else
+    export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
+fi
+
+
+while : ; do
+    echo -n "Deploy solution to project: "$PROJECT_ID" Y/n: "
+    read var_confirm
+    case "$var_confirm" in
+        [yY][eE][sS]|[yY]) 
+            echo "Deploying to $PROJECT_ID"
+            break
+            ;;
+        *)
+            select_project
+            ;;
+    esac
+done
+
 export REGION=$(gcloud config get-value compute/zone)
 regions=()
 num_regions=0
@@ -15,16 +47,25 @@ populate_regions () {
 
 select_region () {
     populate_regions
-    echo "Please select a region."
-    count=0
-    for i in "${regions[@]}"; do
-        echo "[$count]" ${regions[$count]};
-        count=$(expr $count + 1);
+    while : ; do
+        echo "Please select a region."
+        count=0
+        for i in "${regions[@]}"; do
+            echo "[$count]" ${regions[$count]};
+            count=$(expr $count + 1);
+        done
+        echo -n "What region do you want to deploy the reCAPTCHA demo to?: "
+        read var_region
+        # TODO: set the REGION variable based on this answer
+        # include logic to make sure the input makes sense
+        if [ "$num_regions" -gt "$var_region" ]; then
+            REGION="${regions[$var_region]}"
+            break
+        else
+            echo "Selection out of range."
+        fi
     done
-    echo -n "What region do you want to deploy the reCAPTCHA demo to?: "
-    read var_region
-    # TODO: set the REGION variable based on this answer
-    # include logic to make sure the input makes sense
+    
 }
 
 echo -n "Checking region: "
@@ -35,4 +76,20 @@ then
     select_region
 fi
 
-echo ${regions[14]};
+while : ; do
+  echo -n "Deploy reCAPTCHA demo to region: "$REGION" Y/n: "
+    read var_confirm
+    case "$var_confirm" in
+        [yY][eE][sS]|[yY]) 
+            echo "Deploying to $REGION"
+            break
+            ;;
+        *)
+            select_region
+            ;;
+    esac
+done
+
+
+
+echo $PROJECT_ID $REGION;
--- setup.sh
+++ setup.sh
@@ -1,12 +1,93 @@
 #!/bin/sh
 
-export REGION=$(gcloud config get-value compute/zone)
-if [ -z "$REGION" ] then;
-    echo "Region not set";
+select_project () {
+    echo -n "What Project ID do you want to deploy the solution to? (for example my-project-1234): "
+    read var_project_id
+    gcloud config set project $var_project_id
+    export PROJECT_ID=$var_project_id
+    export PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
+}
+
+if [ -z "$GOOGLE_CLOUD_PROJECT" ]
+then
+    echo "Project not set!"
+    select_project
+else
+    export PROJECT_ID=$GOOGLE_CLOUD_PROJECT
 fi
 
+while : ; do
+    echo -n "Deploy solution to project: "$PROJECT_ID" Y/n: "
+    read var_confirm
+    case "$var_confirm" in
+        [yY][eE][sS]|[yY]) 
+            echo "Deploying to $PROJECT_ID"
+            break
+            ;;
+        *)
+            select_project
+            ;;
+    esac
+done
 
+export REGION=$(gcloud config get-value compute/zone)
+regions=()
+num_regions=0
 
+populate_regions () {
+    if [ -z "${regions[0]}" ] 
+    then
+        while IFS= read -r line; do
+            regions+=( "$line" )
+        done < <( gcloud compute regions list --uri | cut -d'/' -f9 )
+        num_regions=$(echo ${#regions[@]})
+    fi
+}
+
+select_region () {
+    populate_regions
+    while : ; do
+        echo "Please select a region."
+        count=0
+        for i in "${regions[@]}"; do
+            echo "[$count]" ${regions[$count]};
+            count=$(expr $count + 1);
+        done
+        echo -n "What region do you want to deploy the reCAPTCHA demo to?: "
+        read var_region
+        # TODO: set the REGION variable based on this answer
+        # include logic to make sure the input makes sense
+        if [ "$num_regions" -gt "$var_region" ]; then
+            REGION="${regions[$var_region]}"
+            break
+        else
+            echo "Selection out of range."
+        fi
+    done
+    
+}
+
+echo -n "Checking region: "
+
+if [ -z "$REGION" ] 
+then
+    echo "Region not set"
+    select_region
+fi
+
+while : ; do
+  echo -n "Deploy reCAPTCHA demo to region: "$REGION" Y/n: "
+    read var_confirm
+    case "$var_confirm" in
+        [yY][eE][sS]|[yY]) 
+            echo "Deploying to $REGION"
+            break
+            ;;
+        *)
+            select_region
+            ;;
+    esac
+done
 
 gcloud services enable recaptchaenterprise.googleapis.com \
     compute.googleapis.com \
@@ -29,7 +110,6 @@ PROJECT_NUMBER=$(gcloud projects list --filter="$(gcloud config get-value projec
 COMMITID=$(git log --format="%H" -n 1)
 SERVICE_ACCOUNT=recaptcha-heroes-compute@$PROJECT_NAME.iam.gserviceaccount.com
 LOG_BUCKET=$PROJECT_NAME-recaptcha-heroes-logs
-
 
 APIKEY=$(gcloud services api-keys create --api-target=service=recaptchaenterprise.googleapis.com --display-name="reCAPTCHA Heroes Demo API key" --format="json" 2>/dev/null | jq '.response.keyString' | cut -d"\"" -f2)
 echo Created an API key for use by reCAPTCHA Enterprise
@@ -55,10 +135,10 @@ gcloud projects add-iam-policy-binding $PROJECT_NUMBER \
     --role='roles/cloudbuild.builds.builder' \
     --role='roles/cloudbuild.serviceAgent' \
     --role='roles/run.developer' \
-    --role='roles/run.admin' \
     --role='roles/run.serviceAgent' \
-    --role='roles/logging.logWriter' \
-    --role='roles/storage.admin'
+    --role='roles/logging.logWriter' 
+    ##--role='roles/storage.admin' \
+    ##--role='roles/run.admin' 
 
 gcloud artifacts repositories create recaptcha-heroes-docker-repo \
     --repository-format=docker \
@@ -67,6 +147,3 @@ gcloud artifacts repositories create recaptcha-heroes-docker-repo \
 echo gcloud builds submit --region=$REGION --config cloudbuild.yaml
 
 echo $0 "done."
-
-
-#ARG iapbackend=0
