4ace76aee04bc75d9308c9375eb1b45abcb38f06
dlenehan <dlenehan@google.com>
2024-11-13 16:01:39 +0000

    Fix testing site keys

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 select_project () {
     echo -n "What Project ID do you want to deploy the solution to? (for example my-project-1234): "
@@ -21,7 +33,7 @@ while : ; do
     echo -n "Deploy solution to project: "$PROJECT_ID" Y/n: "
     read var_confirm
     case "$var_confirm" in
-        [yY][eE][sS]|[yY]|[]) 
+        [yY][eE][sS]|[yY]|"") 
             echo "Deploying to $PROJECT_ID"
             break
             ;;
@@ -80,7 +92,7 @@ while : ; do
   echo -n "Deploy reCAPTCHA demo to region: "$REGION" Y/n: "
     read var_confirm
     case "$var_confirm" in
-        [yY][eE][sS]|[yY]|[]) 
+        [yY][eE][sS]|[yY]|"") 
             echo "Deploying to $REGION"
             break
             ;;
@@ -155,20 +167,15 @@ gcloud artifacts repositories create recaptcha-heroes-docker-repo-$SHORTCOMMIT \
 echo "Starting build"
 gcloud builds submit --region=$REGION --config cloudbuild.yaml 
 
+echo -n "Would you like to connect to the demo now? Y/n: "
+read var_confirm
+case "$var_confirm" in
+    [yY][eE][sS]|[yY]|"") 
+        gcloud run services proxy recaptcha-demo-service-$SHORTCOMMIT --project $PROJECT_ID --region $REGION
+        ;;
+    *)
+        echo "Not connecting to demo."
+        ;;
+esac
 
-
-while : ; do
-  echo -n "Would you like to connect to the demo now? Y/n: "
-    read var_confirm
-    case "$var_confirm" in
-        [yY][eE][sS]|[yY]) 
-            gcloud run services proxy recaptcha-demo-service-$SHORTCOMMIT --project $PROJECT_ID --region $REGION
-            break
-            ;;
-        *)
-            echo "Not connecting to demo."
-            ;;
-    esac
-done
-
-echo "To connect to the demo use: gcloud run services proxy recaptcha-demo-service-$SHORTCOMMIT --project $PROJECT_ID --region $REGION"
+echo "\nTo connect to the demo use: gcloud run services proxy recaptcha-demo-service-$SHORTCOMMIT --project $PROJECT_ID --region $REGION"
